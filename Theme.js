4921c9998ba971eff1e0aa226b2497ac41ec489c
nikola996 <kekev1996@outlook.com>
2017-04-07 15:38:56 +0200

    Add files via upload

---
+++ Theme.js
@@ -0,0 +1,10 @@
+// masonry
+document.write("<script src=\"https://npmcdn.com/masonry-layout@4.0/dist/masonry.pkgd.min.js\"></script>");
+// infinite scroll
+document.write("<script src=\"http://static.tumblr.com/wgijwsy/u2vm2hxv6/jquery.infinitescroll.min.js\"></script>");
+// images loaded
+document.write("<script src=\"http://static.tumblr.com/iwtk77u/Yhym2yygt/jquery.imagesloaded.min.js\"></script>");
+// instagram feed
+document.write("<script src=\"http://www.themesltd.com/assets/js/instagramfd.js\"></script>");
+// font awesome
+document.write("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css\">");
